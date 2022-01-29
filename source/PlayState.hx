3921651848feb3d248802e79ceda115a8293a64e
CharlesCat <62410083+SonicProYT@users.noreply.github.com>
2022-01-29 18:50:20 -0500

    whoops

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2429,6 +2429,14 @@ class PlayState extends MusicBeatState
 		// FlxG.watch.addQuick('VOL', vocals.amplitudeLeft);
 		// FlxG.watch.addQuick('VOLRight', vocals.amplitudeRight);
 
+		var funny:Float = (healthBar.percent * 0.01) + 0.01;
+
+		//icon squish funny haha
+		iconP1.setGraphicSize(Std.int(iconP1.width + (50 * (2 - funny))),Std.int(iconP1.height - (25 * (2 - funny))));
+		iconP2.setGraphicSize(Std.int(iconP2.width + (50 * (2 - funny))),Std.int(iconP2.height - (25 * (2 - funny))));
+
+		iconP1.updateHitbox();
+		iconP2.updateHitbox();
 
 
 		var iconOffset:Int = 26;
@@ -4526,6 +4534,15 @@ class PlayState extends MusicBeatState
 			gf.dance();
 		}
 
+		var funny:Float = (healthBar.percent * 0.01) + 0.01;
+
+		//icon squish funny haha
+		iconP1.setGraphicSize(Std.int(iconP1.width + (50 * (2 - funny))),Std.int(iconP1.height - (25 * (2 - funny))));
+		iconP2.setGraphicSize(Std.int(iconP2.width + (50 * (2 - funny))),Std.int(iconP2.height - (25 * (2 - funny))));
+
+		iconP1.updateHitbox();
+		iconP2.updateHitbox();
+		
 		if(curBeat % 2 == 0) {
 			if (boyfriend.animation.curAnim.name != null && !boyfriend.animation.curAnim.name.startsWith("sing"))
 			{
